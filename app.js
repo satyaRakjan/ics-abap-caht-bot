abe108c1c102e04db5a9d9be53559f6b2a60e275
sattaya037 <sattaya.ra@ics-th.com>
2019-10-24 13:52:10 +0700

    make it better

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const fs = require('fs');
 const line = require('@line/bot-sdk');
 const express = require('express');
-const config = {
-    channelAccessToken: 'B+sgdP8ZDtJblIa3cW4CwVw1uG+wafCGbijMkMzWiO8/LP/UWA3bvbm9oTC4Nm/IDumT/FXNCo4tfwtxz/0UNKgCmcfjXuh1JqUPQEbeOJ03gnO2+Y8cGXx6FiDnAWQwHwv0GSr2XdRblTa5ZejpzQdB04t89/1O/w1cDnyilFU=',
-    channelSecret: 'ddf2d50e4e38a1827824c1d2bbd9afcc'
-};
+const config = require('./config');
 const client = new line.Client(config);
 const app = express();
 const request = require('sync-request')
---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    channelAccessToken: 'B+sgdP8ZDtJblIa3cW4CwVw1uG+wafCGbijMkMzWiO8/LP/UWA3bvbm9oTC4Nm/IDumT/FXNCo4tfwtxz/0UNKgCmcfjXuh1JqUPQEbeOJ03gnO2+Y8cGXx6FiDnAWQwHwv0GSr2XdRblTa5ZejpzQdB04t89/1O/w1cDnyilFU=',
+    channelSecret: 'ddf2d50e4e38a1827824c1d2bbd9afcc'
+  };
+  
+  module.exports = config;
