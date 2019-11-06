a4252670af69876e865bb4d5df67f0d52475ca38
sattaya037 <sattaya.ra@ics-th.com>
2019-11-06 15:29:12 +0700

    make it better

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function handleEvent(event) {
       var a =doc.terms().out('array')
       var dataName = a[0]
       var db = admin.database();
-      var ref = db.ref(dataName+"result");
+      var ref = db.ref(dataName+"/result");
       var lineID =event.source.userId
       var usersRef = ref.child(lineID);
       usersRef.set({
