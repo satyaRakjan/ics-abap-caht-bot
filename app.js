d989447005c717914d3c9dabae5d81f4b00c20b1
sattaya037 <sattaya.ra@ics-th.com>
2019-11-06 14:13:58 +0700

    make it better

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ function handleEvent(event) {
       var db = admin.database();
       var ref = db.ref("vote2");
       ref.on("value", function(snapshot) {
-        console.log(snapshot.val());
+        console.log("test"+snapshot.val());
       }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
       });
