14333b578357a6ce905177d9439a5b03d4ad24e6
sattaya037 <sattaya.ra@ics-th.com>
2019-11-13 15:58:51 +0700

    make it better

--- app.js
+++ app.js
@@ -92,18 +92,19 @@ function handleEvent(event) {
 
 
 function Intent(event){
-  const userName = request("GET", "https://api.line.me/v2/bot/profile/"+event.source.userId, {
+  var userName = request("GET", "https://api.line.me/v2/bot/profile/"+event.source.userId, {
     headers: {
         "Authorization":"Bearer B+sgdP8ZDtJblIa3cW4CwVw1uG+wafCGbijMkMzWiO8/LP/UWA3bvbm9oTC4Nm/IDumT/FXNCo4tfwtxz/0UNKgCmcfjXuh1JqUPQEbeOJ03gnO2+Y8cGXx6FiDnAWQwHwv0GSr2XdRblTa5ZejpzQdB04t89/1O/w1cDnyilFU="
     },
   });
   var userData = JSON.parse(userName.getBody());
-  console.log(userData)
+  var displayName = userData.displayName
+  console.log(displayName)
   const msg = require('./message/messageText');
   const day = require('./message/holiday');
   var doc = nlp(event.message.text)
   var a =doc.terms().out('array')
-  var userSay = a[0];
+  var userSay = a[0]; 
   if(userSay.includes("help>")){
       client.replyMessage(event.replyToken, msg.help);
   }else if(userSay.includes("register>")){
