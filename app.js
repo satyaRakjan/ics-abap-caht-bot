1a4c43e624385c8644098a6391525161a37f99f9
sattaya037 <sattaya.ra@ics-th.com>
2019-11-21 10:35:46 +0700

    first commit

--- app.js
+++ app.js
@@ -161,9 +161,10 @@ function Intent(event){
           ref.on("child_added", function(snapshot, prevChildKey) {
             var newPost = snapshot.val();
             people.push(snapshot.key)
-            console.log(people)
-
+            
           });
+          console.log(people)
+
 
         }
       }, function (errorObject) {
