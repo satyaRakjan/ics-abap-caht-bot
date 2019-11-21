7464eb5148251db73f36164688d54508e2677738
sattaya037 <sattaya.ra@ics-th.com>
2019-11-21 16:15:06 +0700

    first commit

--- app.js
+++ app.js
@@ -172,8 +172,7 @@ function Intent(event){
                 people.push( childSnapshot.key) 
               }});
             var matchc = people.splice(Math.floor(Math.random()*people.length), 1);
-            console.log(matchc[0])
-             ref.child(event.source.userId).child("match").set("matchc")
+             ref.child(event.source.userId).child("match").set(matchc[0])
                 //  return hpy(matchc,event)
           });
     
